e3da975b047c42b8030be6d03aebcfc6f2030585
mostafa abdelsalam <mostafa.abdelsalam96@gmail.com>
2024-06-10 15:22:47 +0300

    Add user column to reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :movie
 
+  validates :user, presence: true
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
   validates :review_text, presence: true
 end
